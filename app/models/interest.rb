e62295559812b0068ebef7bcda2ad078216142a0
MarenWoodruff <wren403@gmail.com>
2014-05-21 16:42:48 -0700

    added validations to users model

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -2,4 +2,9 @@ class Interest
   include Mongoid::Document
   field :username, type: String
   field :email, type: String
+
+  validates :username, presence: true, length: { maximum: 30 },
+                        uniqueness: true
+  validates :email, presence: true, 
+                    uniqueness: true
 end
